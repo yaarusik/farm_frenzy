532605e8e54e7abb2aa702a589ce0d9dadf6db5c
jirafek <nikolaevserafim15@gmail.com>
2022-01-14 22:23:50 +0300

    fix: settings page

--- rsclone/src/application/pages/settingsPage.ts
+++ rsclone/src/application/pages/settingsPage.ts
@@ -13,38 +13,54 @@ export default class SettingsPage extends Control {
 
     const panel = new Control(this.wrapper.node, "div", "panel panel__settings", "");
 
-    const title = new Control(panel.node, "h2", "panel__name-settings", "Настройки");
+    const title = new Control(panel.node, "div", "panel__name-settings", "");
 
     const settingsBox = new Control(panel.node, "div", "settings__box", "");
 
     const soundBox = new Control(settingsBox.node, "div", "sound__box", "");
-    const sound = new Control(soundBox.node, "div", "sound", "Звуки");
-    const soundInp = new Control<HTMLInputElement>(soundBox.node, "input", "sousound__input settings__input", "");
-    soundInp.node.id = "sound";
-    soundInp.node.type = "checkbox";
+    const soundText = new Control(soundBox.node, "div", "sound", "Звуки");
 
-    const soundLabel = new Control<HTMLLabelElement>(soundBox.node, "label", "sousound__label", "");
-    soundLabel.node.setAttribute("for", "sound");
+    const soundBar = new Control(soundBox.node, "div", "sound__bar", "");
 
-    const checkedInputSound = new Control(soundLabel.node, "div", "input__checked", "");
+    const soundMinVolume = new Control(soundBar.node, "div", "sound__volume-min settings__volume", "");
+    this.createRangeInput(soundBar.node, "sound__input settings__range");
+    const soundMaxVolume = new Control(soundBar.node, "div", "sound__volume-max settings__volume", "");
+
+    const soundLine = new Control(soundBar.node, "div", "settings__line", "");
+
 
     const musicBox = new Control(settingsBox.node, "div", "music__box", "");
-    const music = new Control(musicBox.node, "div", "music", "Музыка");
-    const musicInp = new Control<HTMLInputElement>(musicBox.node, "input", "music__input settings__input", "");
-    musicInp.node.id = "music";
-    musicInp.node.type = "checkbox";
+    const musicText = new Control(musicBox.node, "div", "music", "Музыка");
 
-    const musicLabel = new Control<HTMLLabelElement>(musicBox.node, "label", "music__label", "");
-    musicLabel.node.setAttribute("for", "music");
+    const musicBar = new Control(musicBox.node, "div", "music__bar", "");
 
-    const checkedInputMusic = new Control(musicLabel.node, "div", "input__checked", "");
+    const musicMinVolume = new Control(musicBar.node, "div", "music__volume-min settings__volume", "");
+    this.createRangeInput(musicBar.node, "music__input settings__range");
+    const musicMaxVolume = new Control(musicBar.node, "div", "music__volume-max settings__volume", "");
 
-    const mainBackBtn = new Control(panel.node, "button", "btn", "ОК");
+    const musicLine = new Control(musicBar.node, "div", "settings__line", "");
+
+    const fullScreenBox = new Control(settingsBox.node, "div", "screen__box", "На весь экран");
+    const fullScreenCheckBox = new Control<HTMLInputElement>(fullScreenBox.node, "input", "settings__checkbox", "");
+    fullScreenCheckBox.node.id = "fullScreen";
+    fullScreenCheckBox.node.type = "checkbox";
+    const fullScreenLabel = new Control<HTMLLabelElement>(fullScreenBox.node, "label", "fullScreen__label", "");
+    fullScreenLabel.node.setAttribute("for", "fullScreen");
+    const checkedInput = new Control(fullScreenLabel.node, "div", "input__checked", "");
+
+    const mainBackBtn = new Control(panel.node, "button", "btn__settings btn", "ОК");
        mainBackBtn.node.onclick = () => {
           this.onBack();
        };
 
 }
+  createRangeInput(parent: HTMLElement, className: string) {
+    const input = new Control<HTMLInputElement>(parent, "input", className, "");
+    input.node.type = "range";
+    input.node.min = "0";
+    input.node.max = "100";
+    input.node.step = "1";
+  }
 
   onBack() {
     throw new Error("Method not implemented.");
