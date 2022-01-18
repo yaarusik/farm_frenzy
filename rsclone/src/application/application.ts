4660c19a7028ee3d4cf1b80e1affdddb5c2e3ba4
Ruslan <yaarusik@gmail.com>
2022-01-18 22:31:52 +0500

    feat: add animation hover and click

--- rsclone/src/application/application.ts
+++ rsclone/src/application/application.ts
@@ -1,4 +1,4 @@
-import  Control from "../builder/controller";
+import Control from "../builder/controller";
 import StartPage from "./pages/startPage";
 import GameMapPage from "./pages/gameMapPage";
 import ShopPage from "./pages/shopPage";
@@ -8,90 +8,91 @@ import AwardsPage from "./pages/awardsPage";
 import AuthorsPage from "./pages/authorsPage";
 
 export default class Application extends Control {
-   constructor(parentNode: HTMLElement){
-      super(parentNode);
-      // preloader
+	constructor (parentNode: HTMLElement) {
+		super(parentNode);
+		// preloader
 
-      this.mainCycle(); 
-   }
-   // главная страница
-   private mainCycle(){
-      const startPage = new StartPage(this.node);
-      startPage.onSelectMap = () => {
-         startPage.destroy();
-         // preloader
-         this.gameMapCycle();
-      };
-      startPage.onSettings = () => {
-         startPage.destroy();
-         this.settingsCycle();
-      };
-      startPage.onAwards = () => {
-         startPage.destroy();
-         this.awardsCycle();
-      };
-      startPage.onAuthors = () => {
-         startPage.destroy();
-         this.authorsCycle();
-      };
-   }
+		// this.mainCycle(); 
+		this.gameMapCycle();
+	}
+	// главная страница
+	private mainCycle() {
+		const startPage = new StartPage(this.node);
+		startPage.onSelectMap = () => {
+			startPage.destroy();
+			// preloader
+			this.gameMapCycle();
+		};
+		startPage.onSettings = () => {
+			startPage.destroy();
+			this.settingsCycle();
+		};
+		startPage.onAwards = () => {
+			startPage.destroy();
+			this.awardsCycle();
+		};
+		startPage.onAuthors = () => {
+			startPage.destroy();
+			this.authorsCycle();
+		};
+	}
 
 
-   // страница карты
-   private gameMapCycle(){
-      const pageWrapper = new Control<HTMLDivElement>(this.node, "div", "wrapper__map", "");
-      const gameMapPage = new GameMapPage(pageWrapper.node, "div", "map", "");
-      gameMapPage.onSelectShop = () => {
-         gameMapPage.destroy();
-         this.shopCycle();
-      };
-      gameMapPage.startLevel = () => {   
-         gameMapPage.destroy();
-         this.levelCycle();
-      };
-      gameMapPage.onBack = () => {
-         gameMapPage.destroy();
-         this.mainCycle();
-      };
-   }
+	// страница карты
+	private gameMapCycle() {
+		const pageWrapper = new Control<HTMLDivElement>(this.node, "div", "wrapper__map", "");
+		const gameMapPage = new GameMapPage(pageWrapper.node, "div", "map", "");
+		gameMapPage.onSelectShop = () => {
+			gameMapPage.destroy();
+			this.shopCycle();
+		};
+		gameMapPage.startLevel = () => {
+			gameMapPage.destroy();
+			this.levelCycle();
+		};
+		gameMapPage.onBack = () => {
+			gameMapPage.destroy();
+			this.mainCycle();
+		};
+	}
 
-   // страница магазина
-   private shopCycle(){
-      const shopPage = new ShopPage(this.node);
-      shopPage.gameMapBack = () => {
-         shopPage.destroy();
-         this.gameMapCycle();
-      };
-   }
+	// страница магазина
+	private shopCycle() {
+		const shopPage = new ShopPage(this.node);
+		shopPage.gameMapBack = () => {
+			shopPage.destroy();
+			this.gameMapCycle();
+		};
+	}
 
-   private levelCycle(){
-      const levelPage = new LevelPage(this.node);
-      levelPage.gameMapBack = () => {
-         levelPage.destroy();
-         this.gameMapCycle();
-      };
-   }
+	private levelCycle() {
+		const levelPage = new LevelPage(this.node);
+		levelPage.gameMapBack = () => {
+			levelPage.destroy();
+			this.gameMapCycle();
+		};
+	}
 
-   private settingsCycle(){
-      const settingsPage = new SettingsPage(this.node);
-      settingsPage.onBack = () => {
-         settingsPage.destroy();
-         this.mainCycle();      
-      };
-   }
-   private awardsCycle() {
-      const awardsPage = new AwardsPage(this.node);
-      awardsPage.onBack = () => {
-         awardsPage.destroy();
-         this.mainCycle();      
-      };
-   }
+	private settingsCycle() {
+		const settingsPage = new SettingsPage(this.node);
+		settingsPage.onBack = () => {
+			settingsPage.destroy();
+			this.mainCycle();
+		};
+	}
+	private awardsCycle() {
+		const awardsPage = new AwardsPage(this.node);
+		awardsPage.onBack = () => {
+			awardsPage.destroy();
+			this.mainCycle();
+		};
+	}
 
-   private authorsCycle() {
-      const authorsPage = new AuthorsPage(this.node);
-      authorsPage.onBack = () => {
-         authorsPage.destroy();
-         this.mainCycle();      
-      };
-   }
+	private authorsCycle() {
+		const authorsPage = new AuthorsPage(this.node);
+		authorsPage.onBack = () => {
+			authorsPage.destroy();
+			this.mainCycle();
+		};
+	}
 }
--- rsclone/src/application/pages/gameMapPage.ts
+++ rsclone/src/application/pages/gameMapPage.ts
@@ -1,189 +1,325 @@
 import Picture from "../../utils/canvasBtn";
 import CutPicture from "../../utils/cutPictures";
-import  Control from "../../builder/controller";
+import Control from "../../builder/controller";
 
-interface IButtons {
-   type: string, name: string, image: string; x: number; y: number; width: number; height: number; sx: number; sy: number; swidth: number; sheight: number; 
+interface IPictures {
+	type: string; name: string; image: string; x: number; y: number; width: number; height: number; sx: number; sy: number; swidth: number; sheight: number; stepY?: number; id?: number
 }
+
+
 
 export default class GameMapPage extends Control {
-     startLevel!: () => void;
-   onBack!: () => void;
-   onSelectShop!: () => void;
+	startLevel!: () => void;
+	onBack!: () => void;
+	onSelectShop!: () => void;
 
-   heightRatio: number;
-   curWidthK: number;
-   curHeightK: number;
-   imagesOptions: IButtons[];
-   buttons: IButtons[];
+	heightRatio: number;
+	curWidthK: number;
+	curHeightK: number;
+	imagesOptions: IPictures[];
+	buttons: IPictures[];
+	canvas: Control<HTMLCanvasElement>;
+	context: CanvasRenderingContext2D;
 
-   constructor(parentNode: HTMLElement, tagName = "div", className = "", content = ""){
-      super(parentNode, tagName, className, content);
+	constructor (parentNode: HTMLElement, tagName = "div", className = "", content = "") {
+		super(parentNode, tagName, className, content);
 
-      this.imagesOptions = [
-         {  
-            type: 'picture',
-            name: "карта",
-            image: "images/map/map.jpg",
-            x: 0, 
-            y:0,
-            width: 1600,
-            height: 1200, 
-            sx: 0,
-            sy: 0,
-            swidth: 0,
-            sheight: 0
-         },
-         {
-            type: 'picture',
-            name: "меню__бар",
-            image: "images/map/map__menu.png",
-            x: 0, 
-            y: 1092,
-            width: 1600,
-            height: 110,
-            sx: 0,
-            sy: 0,
-            swidth: 0,
-            sheight: 0
-         },
-         {  
-            type: 'button',
-            name: "магазин",
-            image: "images/map/button_3.png",
-            x: 16, 
-            y: 1114,
-            width: 148,
-            height: 62, 
-            sx: 2,
-            sy: 2,
-            swidth: 75,
-            sheight: 37
-         },
-         {
-            type: 'button',
-            name: "меню",
-            image: "images/map/button_3.png",
-            x: 1456, 
-            y: 1114,
-            width: 134,
-            height: 62, 
-            sx: 2,
-            sy: 2,
-            swidth: 75,
-            sheight: 37
-         }, 
-         {
-            type: 'button',
-            name: "уровень1",
-            image: "images/map/new__level.png",
-            x: 590, 
-            y: 958,
-            width: 54,
-            height: 54, 
-            sx: 11,
-            sy: 164,
-            swidth: 54,
-            sheight: 54
-         }
-      ]; 
+		this.imagesOptions = [
+			{
+				type: "picture",
+				name: "карта",
+				image: "images/map/gameMapStart.jpg",
+				x: 0,
+				y: 0,
+				width: 1600,
+				height: 1200,
+				sx: 0,
+				sy: 0,
+				swidth: 0,
+				sheight: 0
+			},
+			{
+				type: "picture",
+				name: "меню__бар",
+				image: "images/map/map__menu.png",
+				x: 0,
+				y: 1092,
+				width: 1600,
+				height: 110,
+				sx: 0,
+				sy: 0,
+				swidth: 0,
+				sheight: 0
+			},
+			{
+				type: "button",
+				name: "магазин",
+				image: "images/map/menuButtons.png",
+				stepY: 44,
+				x: 16,
+				y: 1112,
+				width: 148,
+				height: 68,
+				sx: 0,
+				sy: 0,
+				swidth: 80,
+				sheight: 41,
+			},
+			{
+				type: "button",
+				name: "меню",
+				image: "images/map/menuButtons.png",
+				stepY: 44,
+				x: 1456,
+				y: 1112,
+				width: 136,
+				height: 66,
+				sx: 0,
+				sy: 0,
+				swidth: 80,
+				sheight: 41,
+			},
+			{
+				type: "button",
+				name: "уровень",
+				image: "images/map/new__level.png",
+				id: 1,
+				x: 560,
+				y: 928,
+				width: 76,
+				height: 76,
+				sx: 0,
+				sy: 0,
+				stepY: 76,
+				swidth: 76,
+				sheight: 76,
+			},
+			{
+				type: "button",
+				name: "уровень",
+				image: "images/map/new__level.png",
+				id: 2,
+				x: 635,
+				y: 997,
+				width: 76,
+				height: 76,
+				sx: 0,
+				sy: 0,
+				stepY: 76,
+				swidth: 76,
+				sheight: 76,
+			},
+			{
+				type: "button",
+				name: "уровень",
+				image: "images/map/new__level.png",
+				id: 3,
+				x: 716,
+				y: 1068,
+				width: 76,
+				height: 76,
+				sx: 0,
+				sy: 0,
+				stepY: 76,
+				swidth: 76,
+				sheight: 76,
+			},
+			{
+				type: "button",
+				name: "уровень",
+				image: "images/map/new__level.png",
+				id: 4,
+				x: 805,
+				y: 1014,
+				width: 76,
+				height: 76,
+				sx: 0,
+				sy: 0,
+				stepY: 76,
+				swidth: 76,
+				sheight: 76,
+			},
+			{
+				type: "button",
+				name: "уровень",
+				image: "images/map/new__level.png",
+				id: 5,
+				x: 898,
+				y: 945,
+				width: 76,
+				height: 76,
+				sx: 0,
+				sy: 0,
+				stepY: 76,
+				swidth: 76,
+				sheight: 76,
+			},
+			{
+				type: "button",
+				name: "уровень",
+				image: "images/map/new__level.png",
+				id: 6,
+				x: 995,
+				y: 877,
+				width: 76,
+				height: 76,
+				sx: 0,
+				sy: 0,
+				stepY: 76,
+				swidth: 76,
+				sheight: 76,
+			}
+		];
 
-      this.buttons = this.imagesOptions.filter(btn => btn.type === 'button');
-      // коэффициенты масштаба
-      this.curWidthK = 1;
-      this.curHeightK = 1;
-      this.heightRatio = 1.33333333;
+		this.buttons = this.imagesOptions.filter(btn => btn.type === "button");
+		// коэффициенты масштаба
+		this.curWidthK = 1;
+		this.curHeightK = 1;
+		this.heightRatio = 1.33333333;
 
-      const canvasContainer = new Control(this.node, "div", "canvas__container", "");
-      const canvas = new Control<HTMLCanvasElement>(canvasContainer.node, "canvas", "canvas", "");
-      this.createCanvas(canvas.node);
-      this.scaleNumber(canvas.node);
+		const canvasContainer = new Control(this.node, "div", "canvas__container", "");
+		this.canvas = new Control<HTMLCanvasElement>(canvasContainer.node, "canvas", "canvas", "");
+		this.canvas.node.width = 1600;
+		this.canvas.node.height = 1200;
+		this.context = <CanvasRenderingContext2D>this.canvas.node.getContext("2d");
+		this.startMap();
 
-     
-      window.onresize = () =>{   
-         this.resize(canvas.node);
-         // запоминает масштаб
-         this.scaleNumber(canvas.node);        
-      };   
-  }
+		window.onresize = () => {
+			this.resize(this.canvas.node);
+			this.canvasScale(this.canvas.node);
+		};
 
-      private scaleNumber(canvas: HTMLCanvasElement) {
-         const widthContainer = getComputedStyle(canvas).width;
-         const heightContainer = getComputedStyle(canvas).height;
-         console.log(heightContainer, widthContainer);
-          this.curWidthK = 1600 / parseInt(widthContainer, 10);
-          this.curHeightK = 1200 / parseInt(heightContainer, 10);
-         //  console.log(this.curWidthK, this.curHeightK);
-         // alert(`w: ${widthContainer}, k: ${this.curWidthK}`);
-   }
- 
-   private resize(canvas: HTMLCanvasElement) {
-      canvas.style.height = `${this.heightRatio * canvas.width}`;
-   }
+		this.canvas.node.addEventListener("mousemove", (e) => {
+			this.canvasMoveHundler(e, this.canvas.node, this.buttons);
+		});
+		this.canvas.node.addEventListener("click", (e) => {
+			this.canvasClickHundler(e, this.canvas.node, this.buttons);
+		});
+	}
 
-   private createCanvas(canvas: HTMLCanvasElement) {
-      canvas.addEventListener("click", (e) => {
-            this.canvasEventsHundler(e, canvas, this.buttons);
-         });
-      canvas.width = 1600;
-      canvas.height = 1200;
-      const context = <CanvasRenderingContext2D>canvas.getContext("2d");
+	private startMap() {
+		const loadImages = this.imagesOptions.map(image => this.loadImage(image.image));
+		this.canvasScale(this.canvas.node);
 
-      const pictures = this.imagesOptions.map(image => this.loadImage(image.image));
-      Promise.all(pictures).then(responses => {
-         responses.forEach((img, index)=> {
-           const item = this.imagesOptions[index];
-           if(item.sx){
-            const cutPicture = new CutPicture(img , item.sx, item.sy, item.swidth, item.sheight, item.x, item.y, item.width, item.height);
-            cutPicture.draw(context);
-           } else {
-            const btn = new Picture(img, item.x, item.y, item.width, item.height);
-           btn.draw(context); 
-           }
-         });   
-      });
-   }
+		this.run(loadImages);
+	}
 
-   private loadImage(src: string): Promise<HTMLImageElement> {
-      return new Promise((resolve) =>{
-         const image = new Image();
-         image.src = src;
-         console.log(image.src);
-         image.onload = () => resolve(image);
-        });
-     } 
+	private render(loadImages: Promise<HTMLImageElement>[]) {
+		this.context.clearRect(0, 0, this.canvas.node.width, this.canvas.node.height);
+		this.drawImage(this.context, loadImages);
+	}
 
-   private canvasEventsHundler(e: MouseEvent, canvas: HTMLCanvasElement, buttons: IButtons[]){
-      const rect = canvas.getBoundingClientRect();
-      const mouseX = e.clientX - rect.left;
-      const mouseY = e.clientY - rect.top;
-      console.log(buttons);
-      buttons.forEach(btn => {
-         const currentX = btn.x / this.curWidthK;
-         const currentW = btn.width / this.curWidthK;
-         const currentY = btn.y / this.curHeightK;
-         const currentH = btn.height / this.curHeightK;
-         if(mouseX >= currentX && mouseX < (currentX + currentW) && mouseY >= currentY && mouseY < currentY + currentH) {
-            switch(btn.name){
-               case "магазин": {
-                  this.onSelectShop();
-                  break;
-               }
-               case "меню": {
-                  this.onBack();
-                  break;
-               }
-               case "уровень1": {
-                  this.startLevel();
-                  break;
-               }
-               default: {
-                  console.log("error");
-               }
-            }
-         }
-      });
-     }  
+	private run(loadImages: Promise<HTMLImageElement>[]) {
+		this.render(loadImages);
+
+
+		requestAnimationFrame(() => {
+			this.run(loadImages);
+		});
+	}
+
+	private canvasScale(canvas: HTMLCanvasElement) {
+		const widthContainer = getComputedStyle(canvas).width;
+		const heightContainer = getComputedStyle(canvas).height;
+		this.curWidthK = 1600 / parseInt(widthContainer, 10);
+		this.curHeightK = 1200 / parseInt(heightContainer, 10);
+	}
+
+	private resize(canvas: HTMLCanvasElement): void {
+		canvas.style.height = `${this.heightRatio * canvas.width}`;
+	}
+
+	private drawImage(ctx: CanvasRenderingContext2D, loadImages: Promise<HTMLImageElement>[]) {
+		Promise.all(loadImages).then(responses => {
+			responses.forEach((img, index) => {
+				const item = this.imagesOptions[index];
+				if (item.type === "button") {
+					const cutPicture = new CutPicture(img, item.sx, item.sy, item.swidth, item.sheight, item.x, item.y, item.width, item.height);
+					cutPicture.draw(ctx);
+				} else {
+					const btn = new Picture(img, item.x, item.y, item.width, item.height);
+					btn.draw(ctx);
+				}
+			});
+		});
+	}
+
+	private loadImage(src: string): Promise<HTMLImageElement> {
+		return new Promise((resolve) => {
+			const image = new Image();
+			image.src = src;
+			image.onload = () => {
+				resolve(image);
+			};
+		});
+	}
+
+	private buttonsHover(btn: IPictures, yStep: number) {
+		btn.sy = yStep;
+	}
+
+	private buttonsClick(btn: IPictures, yStep: number) {
+		btn.sy = yStep * 2;
+	}
+
+	private determineCoords(e: MouseEvent, scaleCoords: Coords): boolean {
+		const rect = this.canvas.node.getBoundingClientRect();
+		const mouseX = e.clientX - rect.left;
+		const mouseY = e.clientY - rect.top;
+		const { currentX, currentW, currentY, currentH } = scaleCoords;
+		return mouseX >= currentX && mouseX < (currentX + currentW) && mouseY >= currentY && mouseY < currentY + currentH;
+	}
+
+	private canvasMoveHundler(event: MouseEvent, canvas: HTMLCanvasElement, buttons: IPictures[]) {
+		buttons.forEach(btn => {
+			const scaleCoords: Coords = {
+				currentX: btn.x / this.curWidthK,
+				currentW: btn.width / this.curWidthK,
+				currentY: btn.y / this.curHeightK,
+				currentH: btn.height / this.curHeightK
+			};
+			if (this.determineCoords(event, scaleCoords)) {
+				this.buttonsHover(btn, btn.stepY as number);
+			} else {
+				this.buttonsHover(btn, 0 as number);
+			}
+		});
+	}
+	private canvasClickHundler(event: MouseEvent, canvas: HTMLCanvasElement, buttons: IPictures[]) {
+		buttons.forEach(btn => {
+			const scaleCoords: Coords = {
+				currentX: btn.x / this.curWidthK,
+				currentW: btn.width / this.curWidthK,
+				currentY: btn.y / this.curHeightK,
+				currentH: btn.height / this.curHeightK
+			};
+			if (this.determineCoords(event, scaleCoords)) {
+				switch (btn.name) {
+					case "магазин": {
+						// УБРАТЬ ПРИВЕДЕНИЕ ТИПА
+						this.buttonsClick(btn, btn.stepY as number);
+						setTimeout(this.onSelectShop, 250);
+						break;
+					}
+					case "меню": {
+						this.buttonsClick(btn, btn.stepY as number);
+						setTimeout(this.onBack, 250);
+						break;
+					}
+					case "уровень": {
+						this.buttonsClick(btn, btn.stepY as number);
+						setTimeout(this.startLevel, 250);
+						break;
+					}
+					default: console.log("error");
+				}
+			} else {
+				this.buttonsClick(btn, 0 as number);
+			}
+		});
+	}
 }
 
-
+interface Coords {
+	currentX: number, currentY: number, currentW: number, currentH: number
+}
