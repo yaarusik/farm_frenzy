138ccc56be9a58618a8e102802d37dfb020071ef
YakovLya <yakaulyahovich@gmail.com>
2022-02-17 00:40:06 +0300

    feat: add database connection

--- index.js
+++ index.js
@@ -1,12 +1,25 @@
 const express = require('express')
+const mongoose = require('mongoose');
+
 const app = express();
-const PORT = process.env.PORT || 5000
+const PORT = process.env.PORT || 5000;
 
 app
-  .use(express.static(__dirname))
-  .set('views', __dirname)
-  .set('view engine', 'ejs');
+.use(express.static(__dirname))
+.set('views', __dirname)
+.set('view engine', 'ejs');
 
 app.get('/', (req, res) => res.render('index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
-app.get('/test', (req, res) => res.send('Back test'));
+app.get('/test', (req, res) => res.send(MONGO_CONNECTION_STRING));
+
+mongoose.connect(MONGO_CONNECTION_STRING);
+const connect = mongoose.connection;
+connect.on('connected', function() {
+  console.log('database is connected successfully');
+});
+connect.on('disconnected',function(){
+  console.log('database is disconnected successfully');
+})
+connect.on('error', console.error.bind(console, 'connection error:'));
+module.exports = connect;
