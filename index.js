3ea718dd45fdb4b6b47415d9a9988198927ee30e
YakovLya <yakaulyahovich@gmail.com>
2022-02-16 21:39:30 +0300

    try backend

--- index.js
+++ index.js
@@ -4,8 +4,9 @@ const PORT = process.env.PORT || 5000
 
 app
   .use(express.static(__dirname))
-  .set('view engine', 'ejs')
-  .get('/', (req, res) => res.render('index'))
-  .listen(PORT, () => console.log(`Listening on ${ PORT }`))
+  .set('views', __dirname)
+  .set('view engine', 'ejs');
 
+app.get('/', (req, res) => res.render('index'))
+app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 app.get('/test', (req, res) => res.send('Back test'));
