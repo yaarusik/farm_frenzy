debf39c24d3ba59336922a4b213e3acd8edab4ab
YakovLya <yakaulyahovich@gmail.com>
2022-02-17 19:32:14 +0300

    feat: try backend

--- index.js
+++ index.js
@@ -11,20 +11,27 @@ app
 .set('views', __dirname)
 .set('view engine', 'ejs');
 
+const Schema = mongoose.Schema; 
+const userScheme = new Schema({
+    name: String,
+    password: String
+});
+const User = mongoose.model("User", userScheme);
+
 mongoose.connect(dataURL);
 const connect = mongoose.connection;
-let isConnected = false;
-connect.on('connected', function() {
-  isConnected = true;
-  console.log('database is connected successfully');
-});
-connect.on('disconnected',function(){
-  console.log('database is disconnected successfully');
-})
-connect.on('error', console.error.bind(console, 'connection error:'));
 module.exports = connect;
 
 app.get('/', (req, res) => res.render('index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
-app.post('/', (req, res) => res.send(isConnected));
+app.post('/', (req, res) => {
+  const user = new User({
+    name: "Farmer",
+    password: "12345678"
+  });
+
+  user.save();
+
+  res.json(JSON.stringify(user));
+});
