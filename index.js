ddc3be56809b5219e296408e9c680d1192759d6b
YakovLya <yakaulyahovich@gmail.com>
2022-02-19 17:11:24 +0300

    fix: login

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const dataURL = process.env.MONGO_CONNECTION_STRING;
 app
 .use(express.static(__dirname))
 .set('views', __dirname)
-.set('view engine', 'ejs');
 
 const Schema = mongoose.Schema; 
 const userScheme = new Schema({
@@ -23,7 +22,7 @@ mongoose.connect(dataURL);
 const connect = mongoose.connection;
 module.exports = connect;
 
-app.get('/', (req, res) => res.render('index'))
+app.get('/', (req, res) => res.render('dist/index'))
 app.listen(PORT, () => console.log(`Listening on ${ PORT }`))
 
 app.post('/', async function(req, res) {
